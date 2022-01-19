c42c3b89318aeea3f83e354c1008239c9d0f08c8
jonatasmcampos <camposjonatas3@gmail.com>
2022-01-19 16:20:56 -0300

    mudanca de layout

--- resources/views/componentes/navbar.blade.php
+++ resources/views/componentes/navbar.blade.php
@@ -1,35 +1,56 @@
-{{-- navbar div --}}
-<nav class="containerr" id="navbar">        
-    {{-- ↓↓ logo ↓↓ --}}
+<!-- Sidebar -->
+<nav class="sidebar">
+				
+    <!-- close sidebar menu -->
+    <div class="dismiss">
+        <i class="fas fa-arrow-left"></i>
+    </div>
+    
     <div class="logo">
-        <a href="/"><img src="{{ asset('assets/images/logo.png') }}" alt="Logo"></a>
+        <h3><a href="index.html">Bootstrap 4 Template with Sidebar Menu</a></h3>
     </div>
-    {{-- ↓↓ ancoras para as partes da pagina ↓↓ --}}
-    <ul class="nav-items" id="nav-items">
-        <a href="#inicio">
-            <li class="active">Principal</li>
-        </a>
-        <a href="#introducao">
-            <li>Sobre nos</li>
-        </a>
-        <a href="#pousadas">
-            <li>Pousadas</li>
-        </a>
-        <a href="#rodape">
-            <li>Contato</li>
-        </a>
+    
+    <ul class="list-unstyled menu-elements">
+        <li class="active">
+            <a class="scroll-link" href="#top-content"><i class="fas fa-home"></i> Home</a>
+        </li>
+        <li>
+            <a class="scroll-link" href="#section-1"><i class="fas fa-cog"></i> What we do</a>
+        </li>
+        <li>
+            <a class="scroll-link" href="#section-2"><i class="fas fa-user"></i> About us</a>
+        </li>
+        <li>
+            <a class="scroll-link" href="#section-5"><i class="fas fa-pencil-alt"></i> Portfolio</a>
+        </li>
+        <li>
+            <a class="scroll-link" href="#section-6"><i class="fas fa-envelope"></i> Contact us</a>
+        </li>
+        <li>
+            <a href="#otherSections" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" role="button" aria-controls="otherSections">
+                <i class="fas fa-sync"></i>Other sections
+            </a>
+            <ul class="collapse list-unstyled" id="otherSections">
+                <li>
+                    <a class="scroll-link" href="#section-3">Our projects</a>
+                </li>
+                <li>
+                    <a class="scroll-link" href="#section-4">We think that...</a>
+                </li>
+            </ul>
+        </li>
     </ul>
-    {{-- ↓↓ icones de redes sociais ↓↓ --}}
-    <div class="social-media">
-        <a target="_blank" href="/"><i class="bi bi-linkedin"></i></a>
-        <a target="_blank" href="/"><i class="bi bi-github"></i></a>
+    
+    <div class="to-top">
+        <a class="btn btn-primary btn-customized-3" href="#" role="button">
+            <i class="fas fa-arrow-up"></i> Top
+        </a>
     </div>
-    {{-- ↓↓ toggle menu para clicar quando a tela tiver reduzida ↓↓ --}}
-    <div class="toggle-menu" id="toggle-menu">
-        <div class="" id="line1"></div>
-        <div class="" id="line2"></div>
-        <div class="" id="line3"></div>
-    </div>
-</nav>
+    
+    {{-- <div class="dark-light-buttons">
+        <a class="btn btn-primary btn-customized-4 btn-customized-dark" href="#" role="button">Dark</a>
+        <a class="btn btn-primary btn-customized-4 btn-customized-light" href="#" role="button">Light</a>
+    </div> --}}
 
-{{-- end navbar div --}}
+</nav>
+<!-- End sidebar -->
--- resources/views/estruturaHTML.blade.php
+++ resources/views/estruturaHTML.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -7,26 +7,84 @@
 
     <title>Pousadas Caparaó</title>
 
-    <!-- CSRF Token -->
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{-- importanto favicon --}}
+    
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
-    {{-- importando main.css --}}
+    
     <link rel="stylesheet" href="{{ asset('assets/sass/main.css') }}">
-    {{-- importando bootstrap-icons --}}
+    
     <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
-    {{-- importando bootstrap.css --}}
+    
     <link rel="stylesheet"  href="{{ asset('css/bootstrap.css') }}">
 
 </head>
 
 <body>
-    {{-- importando conteudo da view welcome --}} @yield('conteudo')
+     @yield('conteudo')
 
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="{{ asset('js/jquery.js') }}"></script>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
+</body>
+
+</html> --}}
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Pousadas Caparao</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500&display=swap">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/siedbar/css/jquery.mCustomScrollbar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/siedbar/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/siedbar/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/siedbar/css/media-queries.css') }}">
+
+    <!-- Favicon and touch icons -->
+    <link rel="shortcut icon" href="{{ asset('assets/siedbar/ico/favicon.png') }}">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144"
+        href="{{ asset('assets/siedbar/ico/apple-touch-icon-144-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114"
+        href="{{ asset('assets/siedbar/ico/apple-touch-icon-114-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72"
+        href="{{ asset('assets/siedbar/ico/apple-touch-icon-72-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed"
+        href="{{ asset('assets/siedbar/ico/apple-touch-icon-57-precomposed.png') }}">
+
+</head>
+
+<body>
+    
+    @yield('conteudo')
+
+    <!-- Javascript -->
+    <script src="{{asset('assets/siedbar/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('assets/siedbar/js/jquery-migrate-3.0.0.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="{{asset('assets/siedbar/js/jquery.backstretch.min.js')}}"></script>
+    <script src="{{asset('assets/siedbar/js/wow.min.js')}}"></script>
+    <script src="{{asset('assets/siedbar/js/jquery.waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/siedbar/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script src="{{asset('assets/siedbar/js/scripts.js')}}"></script>
 </body>
 
 </html>
