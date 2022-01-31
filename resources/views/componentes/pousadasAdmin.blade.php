47dd38b83460c66dc02f6ab32573b223230d6f95
jonatasmcampos <camposjonatas3@gmail.com>
2022-01-31 11:05:30 -0300

    add pousada pelo modal

--- resources/views/componentes/pousadasAdmin.blade.php
+++ resources/views/componentes/pousadasAdmin.blade.php
@@ -1,18 +1,62 @@
 <link rel="stylesheet" href="{{ asset('assets/sass/main.css') }}">
 
-<div class="add">
-    <form class="add-pousada" action="{{ route('uploadImg') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <h2> Adicionar pousadas</h2>
-        <input type="text" name="nome" required placeholder="Nome da pousada">
-        <input type="number" name="diaria" required placeholder="Diaria">
-        <input class="descricao" type="text" name="descricao" required placeholder="Descrição">
-        <input type="file" name="image" required class="btn btn-primary btn-customized-2">
-        <button class="btn personalizado" type="submit">Adicionar</button>
-    </form>
-</div>
 
-<br>
+<!-- Modal Introdução --------------------------------------------------- -->
+<div class="modal fade" id="addpousada" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <!-- ↓↓ Titulo do modal ↓↓ -->
+                <p class="mx-auto">
+                    <strong style="font-size: 20px;" class="modal-title" id="exampleModalLabel">
+                        Adicionar Pousadas
+                    </strong>
+                </p>
+                <!-- --------------- -->
+            </div>
+            <!-- ↓↓ Input para alterar titulo principal ↓↓ --------------------- -->
+            <form class="row" action="{{ route('uploadImg') }}" method="POST"
+                enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+
+                            <div class="row">
+                                <input class="campos" type="text" name="nome" required
+                                    placeholder="Nome da pousada">
+                            </div>
+                            <div class="row">
+                                <input class="campos" type="number" step="any" name="diaria" required placeholder="Diaria">
+                                <input class="descricao" type="text" name="descricao" required
+                                    placeholder="Descrição">
+                                <input class="campos" type="file" name="image" required
+                                    class="btn btn-primary btn-customized-2">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- ---------------------------------------------------- -->
+                <!-- ↓↓ Botoes para cancelar ou alterar o titulo principal ↓↓ -->
+                <div class="modal-footer">
+                    <button type="submit" class="btn personalizado">Alterar</button>
+                </div>
+            </form>
+            <!-- --------------------------------------------------------- -->
+
+        </div>
+    </div>
+</div>
+<!-- Final do Modal Subtitulo ----------------------------------------- -->
+
+
+<a class="btn personalizado-2 btn-add" data-toggle="modal" data-target="#addpousada">
+    Adicionar pousadas
+</a>
+
 
 <!-- Section 5 -->
 <div class="section-5-container section-container" id="section-5">
@@ -30,35 +74,32 @@
             <div class="row">
                 @foreach ($pousadas as $pousada)
 
-                <div class="col-md-3 section-5-box wow fadeInUp">
-                    <div class="section-5-box-image">
-                        <img src="{{ $pousada['imagem'] }}" alt="portfolio-1">
-                    </div>
-                    <h3>
-                        <a href="#">{{ $pousada['nome'] }}</a>
-                        <i class="fas fa-angle-right"></i>
-                    </h3>
-                    <div class="section-5-box-date">
-                        Diária: <i>R$</i> {{ $pousada['diaria'] }}
-                    </div>
-                    <p>{{ $pousada['descricao'] }}</p>
-                    <form action="{{ route('info.update', [ 'info' => $pousada['id']])}}" method="POST"
-                        enctype="multipart/form-data">
-                        @method('PUT')
-                        @csrf
-                        <input type="file" name="imageUpdate" required class="btn btn-primary btn-customized-2">
-                        <button type="submit">Alterar</button>
-                    </form>
+                    <div class="col-md-3 section-5-box wow fadeInUp">
+                        <div class="section-5-box-image">
+                            <img src="{{ $pousada['imagem'] }}" alt="portfolio-1">
+                        </div>
+                        <h3>
+                            <a href="#">{{ $pousada['nome'] }}</a>
+                            <i class="fas fa-angle-right"></i>
+                        </h3>
+                        <div class="section-5-box-date">
+                            Diária: <i>R$</i> {{ $pousada['diaria'] }}
+                        </div>
+                        <p>{{ $pousada['descricao'] }}</p>
+                        <form action="{{ route('info.update', ['info' => $pousada['id']]) }}" method="POST"
+                            enctype="multipart/form-data">
+                            @method('PUT')
+                            @csrf
+                            <input type="file" name="imageUpdate" required class="btn btn-primary btn-customized-2">
+                            <button type="submit">Alterar</button>
+                        </form>
 
-                    <a href="{{route('info.destroy', ['info' => $pousada['id']])}}"
-                        data-confirm="Deseja Realmente Retirar Esse Item Da Mesa?" data-method="DELETE"
-                        class="btn btn-primary col-12">Excluir</a>
-                </div>
-
-                <button type="submit">Excluir</button>
+                        <a href="{{ route('info.destroy', ['info' => $pousada['id']]) }}"
+                            data-confirm="Deseja Realmente Retirar Esse Item Da Mesa?" data-method="DELETE"
+                            class="btn btn-primary col-12">Excluir</a>
+                    </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
-</div>
 </div>
