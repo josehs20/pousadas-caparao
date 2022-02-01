754b7921a804c868276f13616cbef6f659eceacd
jonatasmcampos <camposjonatas3@gmail.com>
2022-02-01 11:50:11 -0300

    pagina do usuario acesando uma pousada

---
+++ resources/views/umaPousada.blade.php
@@ -0,0 +1,86 @@
+@extends('estruturaHTML')
+
+@section('conteudo')
+
+    <div class="externo">
+        <div class="div-img-pousada">
+                <!-- Listando as pousadas -->
+                <div class="row">
+                    <div class="div-fotos">
+                        @foreach ($pousadaImgs as $p)
+                            <form action="{{route('listaUmaPousadaUsuario', ['pousada_reg_id' => $p->pousadaReg->id])}}" method="get">
+                                
+                                <button type="submit">
+                                    <img src="{{ Storage::url(substr($p['imagem'], 8)) }}">
+                                    <input name="id" type="hidden" value="{{$p->id}}">
+                                </button>            
+                            </form>
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="row alinhar-pousadas">
+                    <div class="afoto">
+                        
+                        <img src="{{ Storage::url(substr($idFoto['imagem'], 8)) }}">
+                        
+                    </div>
+                    @foreach ($pousadaImgs as $p)
+                        {{-- Adiciona novas imagens e desc pousadas modal --}}
+                        <div class="modal fade" id="addpousada" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+
+                                    <div class="modal-header">
+                                        <!-- ↓↓ Titulo do modal ↓↓ -->
+                                        <p class="mx-auto">
+                                            <strong style="font-size: 20px;" class="modal-title" id="exampleModalLabel">
+                                                Adicione fotos para sua pousada Pousada
+                                            </strong>
+                                        </p>
+
+                                        <!-- --------------- -->
+                                    </div>
+                                    <!-- ↓↓ Input para alterar titulo principal ↓↓ --------------------- -->
+                                    <form class="row"
+                                        action="{{ route('uploadImg', ['pousada_reg_id' => $p['pousada_reg_id']]) }}"
+                                        method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="modal-body">
+                                            <div class="form-row">
+                                                <div class="form-group col-md-12">
+                                                    <div class="row">
+                                                        <input class="campos" type="file" name="image" required
+                                                            class="btn btn-primary btn-customized-2">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- ---------------------------------------------------- -->
+                                        <!-- ↓↓ Botoes para cancelar ou alterar o titulo principal ↓↓ -->
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn personalizado">Adicionar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+        </div>
+        <div class="div-conteudo-pousada">
+            @foreach ( $pousadaImgs as $p )
+                
+            
+            <div>
+                <h1>{{ $p['nome'] }}</h1>
+                <p>{{ $p['diaria'] }}</p>
+
+            </div>
+            <h3>{{ $p['descricao'] }}</h3>
+            @endforeach
+        </div>
+    </div>
+
+@endsection
